52ddfe60c0d15bd77d6d2e72c9bc21d8fbb21e6d
ScProject-Lab <spdev.3141@gmail.com>
2026-01-04 01:37:29 +0900

    add: code

--- js/load_news.js
+++ js/load_news.js
@@ -4,11 +4,12 @@ async function showNews() {
 
     const latest = data.items[0];
 
-    document.getElementById("news_title").textContent =
-        latest.title;
+    const titleEl = document.getElementById("news_title");
+    titleEl.textContent = latest.title;
+    titleEl.href = latest.link;
+    titleEl.target = "_blank";
 
     const date = new Date(latest.pubDate);
-
     document.getElementById("news_meta").textContent =
         `${data.source}｜${date.toLocaleString("ja-JP")}`;
 }
